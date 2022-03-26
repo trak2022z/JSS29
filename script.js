0992d170fa923a78739439aacaaa9ee81b10fe8a
Tom Krok <91782249+trak2022z@users.noreply.github.com>
2022-03-26 09:14:31 +0000

    solution

--- script.js
+++ script.js
@@ -15,24 +15,38 @@
   }
 
   /**
-   * TODO: fill in promise creation
    * Switches to the login view and sets up the systems for submitting and verifying
    * credentials.
    */
   function enterLogin() {
     id('intro').classList.add('hidden');
     id('login').classList.remove('hidden');
+
+    new Promise(loginExecutor)
+      .then(revealSecret)
+      .catch(keepOut);
   }
 
   /**
-   * TODO
    * Resolves the calling Promise when correct credentials are submitted before 10
    * seconds pass, otherwise rejecting when the credentials are submitted.
    * @param {function} resolve - A callback to resolve the promise.
    * @param {function} reject - A callback to reject the promise.
    */
   function loginExecutor(resolve, reject) {
-
+    const TIMEOUT_DELAY = 10000;
+    let timedOut = false;
+    setTimeout(function() {
+      timedOut = true;
+    }, TIMEOUT_DELAY);
+    id('submit-btn').addEventListener('click', function() {
+      if (timedOut || id('username').value !== 'secretStudent' ||
+      id('password').value !== 'secretPassword') {
+        reject();
+      } else {
+        resolve();
+      }
+    });
   }
 
   /**
